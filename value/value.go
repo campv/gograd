8d2804f85b79ad7403905d8be9fb56ff2079842b
campv <vincenzo.campanelli@outlook.com>
2023-08-16 14:55:20 -0400

    tried to use the type constrain Number in the value structure

--- value/value.go
+++ value/value.go
@@ -28,7 +28,20 @@ func (t value) Mul(other value) value {
 	return value{t.Data * other.Data, Tuple[*value, *value]{&t, &other}, "*"}
 }
 
+// Tuple is a generic type that represents a pair of values.
+// The first value is of type T, and the second value is of type U.
 type Tuple[T, U any] struct {
 	First  T
 	Second U
+}
+
+// TODO implement the value struct using generic type for numbers
+// Number is a type that represents a number standard type.
+type Number interface {
+	int64 | float64 | complex128 |
+		int32 | float32 | complex64 |
+		int16 | uint64 |
+		int8 | uint32 |
+		uint16 | uint8 |
+		int | uint
 }
